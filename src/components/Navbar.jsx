4802d012b69c210bc4215a85bb03a6bee2ecd2c7
Christine Peng <caipeng@Christines-Air.hsd1.ma.comcast.net>
2024-12-24 17:29:16 -0500

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,9 +8,30 @@ const Navbar = () => {
     <div className='flex flex-shrink-0 items-center'>
     </div>
     <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
-        <FaLinkedin/>
-        <FaGithub/>
-        <FaInstagram/>
+      <a
+        href="https://www.linkedin.com/in/cai-peng-89407a19a/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="hover:text-blue-500"
+      >
+      <FaLinkedin/> 
+      </a>
+      <a
+        href="https://github.com/CaiPeng99"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="hover:text-gray-700"
+        >
+        <FaGithub />
+      </a>
+      <a
+        href="https://www.instagram.com/cai_peng_pcpc/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="hover:text-pink-500"
+      >
+        <FaInstagram />
+      </a>
     </div>
   </nav>
 }
